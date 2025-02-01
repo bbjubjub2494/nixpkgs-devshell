832302fb4e1bd4b9fd20119da3c30d03cdf39775
Julie B. <no-reply@bbjubjub.fr>
2025-02-01 11:18:00 +0100

    yeet nixpkgs-hammering

--- flake.nix
+++ flake.nix
@@ -4,10 +4,8 @@
   inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.nixpkgs-hammering.url = "github:/jtojnar/nixpkgs-hammering";
-
-  outputs = { self, nixpkgs, devshell, flake-utils, nixpkgs-hammering }:
-    let overlays = [ devshell.overlays.default (final: prev: { inherit (nixpkgs-hammering.packages.${final.system}) nixpkgs-hammering; }) ];
+  outputs = { self, nixpkgs, devshell, flake-utils }:
+    let overlays = [ devshell.overlays.default ];
     in
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit overlays system; }; in
